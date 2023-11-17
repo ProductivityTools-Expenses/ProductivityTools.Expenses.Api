85620f3ac77dee8fde0cbdb20045bb49055317c2
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-11-17 21:17:06 +0100

    expense context

--- ProductivityTools.Expenses.Db/ExpensesContext.cs
+++ ProductivityTools.Expenses.Db/ExpensesContext.cs
@@ -1,11 +1,55 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using ProductivityTools.Expenses.Database.Objects;
 
 namespace ProductivityTools.Expenses.Database
 {
-    public class ExpensesContext
+    public class ExpensesContext: DbContext
     {
+        private readonly IConfiguration configuration;
+
         public DbSet<Expense> Expenses { get; set; }
 
+        public TransfersContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+                   builder.AddConsole()
+                          .AddFilter(DbLoggerCategory.Database.Command.Name,
+                                     LogLevel.Information));
+            return serviceCollection.BuildServiceProvider()
+                    .GetService<ILoggerFactory>();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("PTTransfers"));
+                optionsBuilder.UseLoggerFactory(GetLoggerFactory());
+                optionsBuilder.EnableSensitiveDataLogging();
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Expense>().ToTable("Expenses")
+                .HasKey(x => x.ExpenseId);
+            //modelBuilder.Entity<Account>().HasMany(x => x.TransfersSource).WithOne(x => x.Source).HasForeignKey(x => x.SourceId).HasPrincipalKey(x => x.AccountId);
+            //modelBuilder.Entity<Account>().HasMany(x => x.TransfersTarget).WithOne(x => x.Target).HasForeignKey(x => x.TargetId).HasPrincipalKey(x => x.AccountId);
+          
+            //modelBuilder.Entity<TransferHistory>().ToTable("TransferHistory")
+            //    .HasKey(x => x.TransferHistoryId);
+            //modelBuilder.Entity<Account>().ToTable("Account")
+            //    .HasKey(x => x.AccountId);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
