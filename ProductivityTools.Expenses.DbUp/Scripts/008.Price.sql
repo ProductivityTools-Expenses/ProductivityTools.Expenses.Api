20d42b7715e5c84699c0759f1778a49f01099a51
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-12-14 00:12:25 +0100

    expense

---
+++ ProductivityTools.Expenses.DbUp/Scripts/008.Price.sql
@@ -0,0 +1,10 @@
+﻿
+ALTER TABLE me.Expense drop column Cost
+ALTER TABLE me.Expense drop column value
+
+update [me].[Expense] set price=0 where price is null
+ALTER TABLE me.Expense alter column price DECIMAL(7,2) not null
+EXEC sp_rename 'me.Expense.price', 'Price', 'COLUMN';
+
+ALTER TABLE me.Expense Add Cost as Amount*Price+Additions-Deductions
+alter table me.expense add [Value] as amount*price
