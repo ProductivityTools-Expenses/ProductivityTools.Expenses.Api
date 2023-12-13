009912b8209ccb2536d6acad6f89cad5688f4963
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-12-14 00:07:29 +0100

    column rename

--- ProductivityTools.Expenses.DbUp/Scripts/007.Additions.sql
+++ ProductivityTools.Expenses.DbUp/Scripts/007.Additions.sql
@@ -1,3 +1,9 @@
 ﻿update me.expense set Additions=0 where additions is null
 
+ALTER TABLE me.Expense drop column Cost
+ALTER TABLE me.Expense drop column value
+
 ALTER TABLE me.Expense alter column Additions DECIMAL(7,2) not null
+
+ALTER TABLE me.Expense Add Cost as Amount*Price+Additions-Deductions
+alter table me.expense add [Value] as amount*price
