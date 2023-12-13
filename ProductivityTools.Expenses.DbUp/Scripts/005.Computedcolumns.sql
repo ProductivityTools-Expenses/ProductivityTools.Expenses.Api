c711fd50f402d146c22c54035fbbbe5f1d981184
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-12-13 23:55:01 +0100

    computed columns

---
+++ ProductivityTools.Expenses.DbUp/Scripts/005.Computedcolumns.sql
@@ -0,0 +1,6 @@
+﻿--UPDATE [me].[Expense] SET Amount=1 WHERE Amount IS NULL
+--ALTER TABLE me.Expense ALTER COLUMN Amount int not null
+--EXEC sp_rename 'me.Expense.Value', 'Price', 'COLUMN';
+--ALTER TABLE me.Expense Add [Value] as Amount*Price
+
+
