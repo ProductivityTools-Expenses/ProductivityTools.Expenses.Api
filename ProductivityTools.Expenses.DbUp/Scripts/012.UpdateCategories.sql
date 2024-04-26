ca52929277153adaf6ea95d2973e6c8dbf4bed6b
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-04-26 08:43:28 +0200

    migration of categories

---
+++ ProductivityTools.Expenses.DbUp/Scripts/012.UpdateCategories.sql
@@ -0,0 +1,14 @@
+﻿update e set e.CategoryID=n.newCategoryId
+from me.Expense e
+inner join(
+SELECT e.ExpenseID as expenseId
+      ,e.[CategoryID] as oldCategoryId
+      ,e.[BagID] as bagId
+	  ,c1.Name as Name
+	  ,c2.CategoryID as newCategoryId
+  FROM [PTExpenses].[me].[Expense] e
+  inner join me.Category c1 on e.CategoryID=c1.CategoryID
+  inner join me.Category c2 on c1.Name=c2.Name 
+  where c1.CategoryID!=c2.CategoryID
+  and c2.BagId=e.BagID) n
+  on n.expenseId=e.ExpenseID
