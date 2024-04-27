2a0024d9c981e2d60b04cd0c1789f7916b494a45
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-04-27 21:25:46 +0200

    update income

--- ProductivityTools.Expenses.DbUp/Scripts/015.UpdateIncome.sql
+++ ProductivityTools.Expenses.DbUp/Scripts/015.UpdateIncome.sql
@@ -1,14 +1,15 @@
-﻿update e set e.CategoryID=n.newCategoryId
-from me.Expense e
+﻿
+  update e set e.CategoryID=n.newCategoryId
+from me.Income e
 inner join(
-SELECT e.ExpenseID as expenseId
+SELECT e.IncomeId as expenseId
       ,e.[CategoryID] as oldCategoryId
       ,e.[BagID] as bagId
 	  ,c1.Name as Name
 	  ,c2.CategoryID as newCategoryId
-  FROM [PTExpenses].[me].[Expense] e
+  FROM [PTExpenses].[me].Income e
   inner join me.Category c1 on e.CategoryID=c1.CategoryID
   inner join me.Category c2 on c1.Name=c2.Name 
   where c1.CategoryID!=c2.CategoryID
   and c2.BagId=e.BagID) n
-  on n.expenseId=e.ExpenseID
+  on n.expenseId=e.IncomeID
